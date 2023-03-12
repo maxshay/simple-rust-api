bcedf929b6c5f1a1a5b0e344125fedb3163e5d29
Maksim Shaynyuk <35791051+maxshay@users.noreply.github.com>
2023-03-12 15:22:46 -0700

    update message on startup, restart api after build

--- src/main.rs
+++ src/main.rs
@@ -7,8 +7,13 @@ use actix_web::{App, HttpServer};
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     let port = std::env::var("SERVER_PORT").expect("SERVER_PORT must be set");
-    HttpServer::new(|| App::new().service(index).service(compute_fib))
-        .bind(("127.0.0.1", port.parse::<u16>().unwrap()))?
-        .run()
-        .await
+    let server = HttpServer::new(|| App::new().service(index).service(compute_fib))
+        .bind(("127.0.0.1", port.parse::<u16>().unwrap()))?;
+
+    println!(
+        "Starting `Simple Rust Api` server, listening on port {}",
+        port
+    );
+
+    server.run().await
 }
